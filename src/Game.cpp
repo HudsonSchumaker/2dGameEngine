d68c9e35bcc16af0676d7b7e378f5d1947375933
HudsonSchumaker <hudson.schumaker@me.com>
2021-12-30 23:36:56 +0100

    first commit

---
+++ src/Game.cpp
@@ -0,0 +1,33 @@
+#include "Game.hpp"
+
+Game::Game() {
+
+}
+
+Game::~Game() {
+
+}
+
+void Game::Initialize() {
+
+}
+
+void Game::Run() {
+
+}
+
+void Game::ProcessInput() {
+
+}
+
+void Game::Update() {
+
+}
+
+void Game::Render() {
+
+}
+
+void Game::Destroy() {
+
+}
---
+++ src/Main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#include <glm/glm.hpp>
+#include <imgui/imgui.h>
+#include <sol/sol.hpp>
+#include "Game.hpp"
+
+int main(int argc, char* argv[]) {
+    //SDL_Init(SDL_INIT_EVERYTHING);
+    //glm::vec2 velocity = glm::vec2(5.5, -2.0);
+    //sol::state lua;
+    //lua.open_libraries(sol::lib::base);
+    //std::cout << "Hello, world!" << std::endl;
+
+    Game game;
+    game.Initialize();
+    game.Run();
+    game.Destroy();
+
+
+    return 0;
+}
+
