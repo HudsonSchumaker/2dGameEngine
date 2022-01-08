afd3f86f4b241ccba9bcc856306d2c768cdd8544
HudsonSchumaker <hudson.schumaker@me.com>
2022-01-08 20:41:17 +0100

    not working... :-(

--- src/ECS/RegistryEntity.hpp
+++ src/ECS/RegistryEntity.hpp
@@ -118,4 +118,47 @@ template <typename S> S& Registry::GetSystem() const {
     return *(std::static_pointer_cast<S>()(system->second));
 }
 
+/*
+ */
+
+class Entity final {
+private:
+    int id;
+
+public:
+    Entity(int id): id(id) {};
+    Entity(const Entity& entity) = default;
+    ~Entity() = default;
+    int GetId() const;
+
+    Entity& operator =(const Entity& other) = default;
+    bool operator ==(const Entity& other) const { return id == other.id; }
+    bool operator !=(const Entity& other) const { return id != other.id; }
+    bool operator >(const Entity& other) const { return id > other.id; }
+    bool operator <(const Entity& other) const { return id < other.id; }
+
+    template <typename C, typename ...CArgs> void AddComponent(CArgs&& ...args);
+    template <typename C> void RemoveComponent();
+    template <typename C> bool HasComponent() const;
+    template <typename C> C& GetComponent() const;
+
+    // class Registry* registry; // do better
+};
+
+template <typename C, typename ...CArgs> void Entity::AddComponent(CArgs&& ...args) {
+    Registry::getInstance().AddComponent<C>(*this, std::forward<CArgs>(args)...);
+}
+
+template <typename C> void Entity::RemoveComponent() {
+    //Registry::getInstance().RemoveComponent<C>(*this);
+}
+
+template <typename C> bool Entity::HasComponent() const {
+    return false; //Registry::getInstance()::HasComponent<C>(*this);
+}
+
+template <typename C> C& Entity::GetComponent() const {
+    return nullptr;// Registry::getInstance().GetComponent<C>(*this);
+}
+
 #endif /* Registry_hpp */
