f8e3d0e197c04b143161e6a5e56980a7becfa9d5
HudsonSchumaker <hudson.schumaker@me.com>
2022-06-16 21:22:28 +0200

    ST-LAB-HS engine

---
+++ src/headers/Basic.hpp
@@ -0,0 +1,25 @@
+//
+// SchumakerLab
+// Dodoi Engine
+// Hudson Schumaker
+//
+
+#pragma once
+#include <map>
+#include <array>     // std::array
+#include <math.h>
+#include <vector>
+#include <random>    // std::default_random_engine
+#include <chrono> 
+#include <future>
+#include <string>
+#include <utility>   // std:pair<T,T>
+#include <stdlib.h>
+#include <unistd.h>
+#include <iostream>
+#include <algorithm> // std::shuffle
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
