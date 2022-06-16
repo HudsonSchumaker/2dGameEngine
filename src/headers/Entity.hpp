7d605bdd6832f8d82a93b7daa943a27d1bdde2b2
HudsonSchumaker <hudson.schumaker@me.com>
2022-06-16 21:22:43 +0200

    ST-LAB-HS engine

---
+++ src/headers/Entity.hpp
@@ -0,0 +1,15 @@
+//
+// SchumakerLab
+// Dodoi Engine
+// Hudson Schumaker
+//
+
+#pragma once
+
+namespace dodoi {
+    class Entity {
+        public:
+            Entity() = default;
+            short id;
+    };
+}
