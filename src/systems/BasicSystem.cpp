e99734fd6e9de28617be513da9cdc7444c2aea83
HudsonSchumaker <hudson.schumaker@me.com>
2022-06-16 22:02:59 +0200

    ST-LAB-HS engine

---
+++ src/systems/BasicSystem.cpp
@@ -0,0 +1,13 @@
+//
+// SchumakerLab
+// Dodoi Engine
+// Hudson Schumaker
+//
+
+#include "../headers/BasicSystem.hpp"
+
+using namespace dodoi;
+
+void BasicSystem::addEntity(Entity entity) {
+    entities.push_back(entity);
+}
