0b798a942f9b3c58ac051232f35b7e5e72e14e7c
HudsonSchumaker <hudson.schumaker@me.com>
2022-06-19 22:36:40 +0200

    ST-SL-HS engine

--- src/headers/Vec2.hpp
+++ src/headers/Vec2.hpp
@@ -9,42 +9,42 @@
 
 namespace dodoi {
     struct Vec2 {
-    float x;
-    float y;
+        float x;
+        float y;
 
-    Vec2();
-    Vec2(float x, float y);
-    ~Vec2() = default;
+        Vec2();
+        Vec2(float x, float y);
+        ~Vec2() = default;
 
-    void add(const Vec2& v);
-    void sub(const Vec2& v);
-    void scale(const float n);
-    Vec2 rotate(const float angle) const;
+        void add(const Vec2& v);
+        void sub(const Vec2& v);
+        void scale(const float n);
+        Vec2 rotate(const float angle) const;
     
-    float magnitude() const;
-    float magnitudeSquared() const;
+        float magnitude() const;
+        float magnitudeSquared() const;
     
-    Vec2& normalize();
-    Vec2 unitVector() const;
-    Vec2 normal() const;
+        Vec2& normalize();
+        Vec2 unitVector() const;
+        Vec2 normal() const;
     
-    float dot(const Vec2& v) const;
-    float cross(const Vec2& v) const;
+        float dot(const Vec2& v) const;
+        float cross(const Vec2& v) const;
 
-    Vec2& operator = (const Vec2& v);
-    bool operator == (const Vec2& v) const;
-    bool operator != (const Vec2& v) const;
+        Vec2& operator = (const Vec2& v);
+        bool operator == (const Vec2& v) const;
+        bool operator != (const Vec2& v) const;
 
-    Vec2 operator + (const Vec2& v) const;
-    Vec2 operator - (const Vec2& v) const;
-    Vec2 operator * (const float n) const;
-    Vec2 operator / (const float n) const;
+        Vec2 operator + (const Vec2& v) const;
+        Vec2 operator - (const Vec2& v) const;
+        Vec2 operator * (const float n) const;
+        Vec2 operator / (const float n) const;
 
-    Vec2& operator += (const Vec2& v);
-    Vec2& operator -= (const Vec2& v);
-    Vec2& operator *= (const float n);
-    Vec2& operator /= (const float n);
+        Vec2& operator += (const Vec2& v);
+        Vec2& operator -= (const Vec2& v);
+        Vec2& operator *= (const float n);
+        Vec2& operator /= (const float n);
 
-    Vec2 operator - ();
+        Vec2 operator - ();
     };
 }
