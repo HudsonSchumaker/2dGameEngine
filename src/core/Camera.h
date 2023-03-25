158e9d068331f308144d508c31e075577f867739
HudsonSchumaker <hudson.schumaker@me.com>
2023-03-25 20:32:20 +0100

    add camera

---
+++ src/core/Camera.h
@@ -0,0 +1,29 @@
+/*
+	Windows 10 - 11
+	SchumakerTeam
+	Hudson Schumaker
+*/
+
+#pragma once
+#include "../include/Common.h"
+
+class Camera final {
+public:
+	int x, y, w, h;
+
+	Camera() {
+		x = 0;
+		y = 0;
+		w = 0;
+		h = 0;
+	}
+
+	Camera(int x, int y, int w, int h) {
+		this->x = x;
+		this->y = y;
+		this->w = w;
+		this->h = h;
+	}
+
+	~Camera() = default;
+};
