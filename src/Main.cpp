c82af0812338e45fa66f1bb756437746bf562a5a
HudsonSchumaker <hudson.schumaker@me.com>
2023-03-26 01:11:16 +0100

    Main file

---
+++ src/Main.cpp
@@ -0,0 +1,34 @@
+/*
+    SchumakerTeam
+    Hudson Schumaker
+*/
+
+#include "gfx/Gfx.h"
+#include "gfx/AssetManager.h"
+#include "sfx/AudioManager.h"
+
+int main(int argc, char* argv[]) {
+    setUp();
+    load();
+
+    return 0;
+}
+
+void setUp() {
+    Gfx::getInstance()->setGfxContext();
+}
+
+void load() {
+    AssetManager::getInstance()->load();
+    AudioManager::getInstance()->load();
+}
+
+void quit() {
+    SDL_GameControllerClose(0);
+    IMG_Quit();
+    TTF_Quit();
+    Mix_HaltChannel(-1);
+    Mix_CloseAudio();
+    Mix_Quit();
+    SDL_Quit();
+}
