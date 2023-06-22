cd892cb9f30f1f6f66e83f169cd2e79f90004be3
HudsonSchumaker <hudson.schumaker@me.com>
2023-06-22 22:35:06 +0200

    refactor

--- src/ecs/system/RenderTextSystem.cpp
+++ src/ecs/system/RenderTextSystem.cpp
@@ -1,5 +1,6 @@
 /*
-	SchumakerTeam
+	Dodoi Lab
+	Dodoi Engine
 	Hudson Schumaker
 */
 
@@ -8,12 +9,18 @@
 #include "../EntityManager.h"
 #include "../component/TextLabel.h"
 #include "../component/Transform.h"
+#include "../component/SpriteText.h"
 
 RenderTextSystem::RenderTextSystem() {
 	this->renderer = Gfx::getInstance()->getRenderer();
 }
 
 void RenderTextSystem::update(Camera* camera) {
+	renderTextLabel(camera);
+	renderSpriteText(camera);
+}
+
+void RenderTextSystem::renderTextLabel(Camera* camera) {
 	auto entities = EntityManager::getInstance()->getEntitiesWithComponent<TextLabel>();
 
 	for (auto& entity : entities) {
@@ -29,6 +36,33 @@ void RenderTextSystem::update(Camera* camera) {
 			
 			// TODO: why not using Gfx?
 			SDL_RenderCopy(renderer, textLabel->label, NULL, &dstRect);
+		}
+	}
+}
+
+void RenderTextSystem::renderSpriteText(Camera* camera) {
+	auto entities = EntityManager::getInstance()->getEntitiesWithComponent<SpriteText>();
+
+	for (auto& entity : entities) {
+		Transform* transform = entity->getComponent<Transform>();
+		SpriteText* spriteText = entity->getComponent<SpriteText>();
+
+		if (transform && spriteText) {
+			int newX = transform->position.x + spriteText->offSet.x;
+			int newY = transform->position.y + spriteText->offSet.y; 
+
+			spriteText->position.x = newX;
+			spriteText->position.y = newY;
+
+			SDL_Rect dstRect = {
+				static_cast<int>(spriteText->position.x - (spriteText->isFixed ? 0 : camera->x)),
+				static_cast<int>(spriteText->position.y - (spriteText->isFixed ? 0 : camera->y)),
+				spriteText->w,
+				spriteText->h
+			};
+			
+			// TODO: why not using Gfx?
+			SDL_RenderCopy(renderer, spriteText->label, NULL, &dstRect);
 		}
 	}
 }
