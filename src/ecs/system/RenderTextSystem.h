2ac020db56808a380e117042d76bcd5f8178715f
HudsonSchumaker <hudson.schumaker@me.com>
2023-06-22 22:35:13 +0200

    refactor

--- src/ecs/system/RenderTextSystem.h
+++ src/ecs/system/RenderTextSystem.h
@@ -10,6 +10,8 @@
 class RenderTextSystem final : public System {
 private:
 	SDL_Renderer* renderer = nullptr;
+	void renderTextLabel(Camera* camera);
+	void renderSpriteText(Camera* camera);
 
 public:
 	RenderTextSystem();
