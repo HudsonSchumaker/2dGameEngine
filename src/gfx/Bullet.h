725154cd2e86853c16de041774cd25c18c9e56f3
HudsonSchumaker <hudson.schumaker@me.com>
2023-07-06 22:11:00 +0200

    add bullet gfx primitive

---
+++ src/gfx/Bullet.h
@@ -0,0 +1,16 @@
+/*
+	Dodoi Lab
+	Dodoi Engine
+	Hudson Schumaker
+*/
+
+#pragma once
+#include "../gfx/Color.h"
+#include "../ecs/Entity.h"
+#include "../physics/Vec2.h"
+#include "../physics/Dimension.h"
+#include "../ecs/component/Component.h"
+
+class Bullet : public Component {
+
+};
