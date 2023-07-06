3c45372a1e6148edcf0f1a91ad975195644f6fb9
HudsonSchumaker <hudson.schumaker@me.com>
2023-07-06 22:10:42 +0200

    fix bullet creation

--- src/core/BulletFactoty.cpp
+++ src/core/BulletFactoty.cpp
@@ -5,7 +5,7 @@
 */
 
 #include "BulletFactory.h"
-#include "../gfx/Animation.h"
+#include "../gfx/Bullet.h"
 #include "../ecs/EntityManager.h"
 #include "../ecs/component/RigidBody.h"
 #include "../ecs/component/Transform.h"
@@ -21,32 +21,35 @@ BulletFactory* BulletFactory::getInstance() {
 }
 
 Entity* BulletFactory::createBullet(BulletType bulletType, bool isFriendly) {
-
-	if (bulletType == BulletType::basic) {
-		Bullet1 b1 = {};
-		return createBullet(b1.id, b1.velocity, isFriendly, b1.damage);
+	if (bulletType == BulletType::BASIC) {
+		BasicBullet b1 = {};
+		return createBullet(b1.velocity, isFriendly, b1.damage);
 	}
 
-	if (bulletType == BulletType::plus) {
-		Bullet2 b2 = {};
-		return createBullet(b2.id, b2.velocity, isFriendly, b2.damage);
+	if (bulletType == BulletType::PLUS) {
+		PlusBullet b2 = {};
+		return createBullet(b2.velocity, isFriendly, b2.damage);
 	}
 
-	if (bulletType == BulletType::master) {
-		Bullet3 b3 = {};
-		return createBullet(b3.id, b3.velocity, isFriendly, b3.damage);
+	if (bulletType == BulletType::MASTER) {
+		MasterBullet b3 = {};
+		return createBullet(b3.velocity, isFriendly, b3.damage);
+	}
+
+	if (bulletType == BulletType::ULTRA) {
+		UltraBullet b3 = {};
+		return createBullet(b3.velocity, isFriendly, b3.damage);
 	}
 
 	return EntityManager::getInstance()->createEntity();
 }
 
-Entity* BulletFactory::createBullet(std::string id, Vec2 velocity, short damage, bool isFriendly, int duration) {
+Entity* BulletFactory::createBullet(Vec2 velocity, short damage, bool isFriendly, int duration) {
 	auto bullet = EntityManager::getInstance()->createEntity();
 	bullet->tag = Tag::bullet;
 
 	bullet->addComponent(new Transform(-10.0f));
-	bullet->addComponent(new Animation(id, 5, 8, (int)Tag::bullet, true));
-	auto animation = bullet->getComponent<Animation>();
+
 	bullet->addComponent(new BoxCollider(animation->getSize().w, animation->getSize().h));
 	bullet->addComponent(new RigidBody(velocity));
 	bullet->addComponent(new Projectile(isFriendly, damage, duration));
