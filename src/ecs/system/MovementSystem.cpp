be64896ad439366c4cf501fa7e1952edc1fa9745
HudsonSchumaker <hudson.schumaker@me.com>
2023-07-06 23:27:00 +0200

    Fix MovementSystem

--- src/ecs/system/MovementSystem.cpp
+++ src/ecs/system/MovementSystem.cpp
@@ -7,6 +7,7 @@
 #include "MovementSystem.h"
 #include "../EntityManager.h"
 #include "../component/Waypoint.h"
+#include "../component/Projectile.h"
 #include "../component/Transform.h"
 #include "../component/RigidBody.h"
 
@@ -14,14 +15,20 @@ void MovementSystem::update(float dt) {
 	auto entities = EntityManager::getInstance()->getEntitiesWithComponent<RigidBody>();
 
 	for (auto& entity : entities) {
+		if (entity->getComponent<Waypoint>()) {
+			continue;
+		}
+
+		if (entity->getComponent<Projectile>()) {
+			continue;
+		}
+
 		RigidBody* rigidBody = entity->getComponent<RigidBody>();
 		Transform* transform = entity->getComponent<Transform>();
 
-		if (!entity->getComponent<Waypoint>()) {
-			if (rigidBody && transform) {
-				transform->position.x += rigidBody->velocity.x * dt;
-				transform->position.y += rigidBody->velocity.y * dt;
-			}
+		if (rigidBody && transform) {
+			transform->position.x += rigidBody->velocity.x * dt;
+			transform->position.y += rigidBody->velocity.y * dt;
 		}
 	}
 }
